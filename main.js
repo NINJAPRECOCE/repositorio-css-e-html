98d9f1fa8c197875eb18d3929efd621db2a01305
gustavo de paula zampieri <128715598+NINJAPRECOCE@users.noreply.github.com>
2024-04-03 08:44:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -14,6 +14,26 @@ for(let i=0;i <botoes.length;i++){
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo= new Date("2024-03-25T00:00:00)
+const tempoObjetivo1= new Date("2025-01-21T00:00:00);
+const tempoObjetivo2= new Date("2026-12-01T00:00:00);
+const tempoObjetivo3= new Date("2027-06-01T00:00:00);
+const tempoObjetivo4= new Date("2028-01-21T00:00:00);
 let tempoAtual=new Date( );
-contadores[0].textContent = tempoObjetivo1-tempoAtual;
+
+const tempos=[tempoObjetivo1, TempoObjetivo2, TempoObjetivo3, TempoObjetivo4]
+contadores[0].textContent = CalculaTempo-(Tempos[0]);
+
+function calculaTempo(tempoObjetivo){
+  let tempoAtual= newDate();
+  let tempoFinal= tempoObjetivo - tempoAtual;
+  let segundos= Math.floor(tempoFinal / 1000);
+  let minutos= Math.floor(segundos / 60);
+  let horas= Math.floor(minutos / 60);
+  let dias= Math.floor(horas / 24);
+
+  segundos %= 60;
+  minutos %=60;
+  horas %=24;
+  return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+ 
+}
